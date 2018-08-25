b2996ff2753c788ec4a142110765f2979730a8c2
julio.zingari <julio.zingari@hotmail.com>
2018-08-25 14:24:58 -0400

    Update do read-me

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps - Jzingari")
+        self.assertRegex(result.data.decode(), "Jzingari - Testando DevOps.")
 
 
 if __name__ == "__main__":

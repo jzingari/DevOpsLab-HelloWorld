b08313862a7edfe5011f7ddfb2ae30bfad8b1d0a
julio.zingari <julio.zingari@hotmail.com>
2018-08-25 16:31:30 -0400

    Update foto

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Jzingari - Testando DevOps.")
+        self.assertRegex(result.data.decode(), "Currículo criado com: Codeanyware - travis-ci - github - heroku")
 
 
 if __name__ == "__main__":
